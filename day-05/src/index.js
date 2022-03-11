077ab0439b7e2704cf79cf36c9babaf15c9384a5
Subramanian Murugan <sasubramanian_md@hotmail.com>
2022-03-11 16:37:21 +0530

    component life cycle pure components

--- day-05/src/index.js
+++ day-05/src/index.js
@@ -1,109 +1,61 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import ReactDOM from 'react-dom'
 
-class ParentComponent extends Component {
-    state = {
-        i: 0
+class CounterButton extends React.Component {
+
+    state = { count: 1 };
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('Current State=>', this.state)
+        console.log("Next State =>", nextState)
+        if (nextState.count !== this.state.count) {
+            return true
+        }
+        return false
     }
-    //mount phase methods
-    constructor() {
-        super()
-        console.log('Parent Constructor is called')
-        // this.state = {
-        //     i: 0
-        // }
-    }
+
     render() {
-        console.log('Parent render is called')
-
-        return <div>
-            <h1>Parent Component</h1>
-            <button onClick={() => {
-                this.setState({ i: this.state.i + 1 })
-            }}>TriggerUpdate</button>
-            <ChildComponent />
-        </div>
+        console.log('render is called')
+        return (
+            <>
+                <button
+                    onClick={() => this.setState(state => ({ count: state.count + 1 }))}>
+                    Count: {this.state.count}
+                </button>
+                <button
+                    onClick={() => this.setState(state => ({ count: state.count }))}>
+                    Count: {this.state.count}
+                </button>
+            </>
+        );
     }
-    static getDerivedStateFromProps(props, state) {
-        console.log('Parent getDerivedStateFromProps is called')
-        return null;
-    }
-    componentDidMount() {
-        console.log('Parent ComponentDidMount is called')
-    }
-
-
 }
 
-class ChildComponent extends Component {
+class CounterPureButton extends React.PureComponent {
 
-    state = {
-        j: 0
-    }
-    //mount phase methods
-    constructor() {
-        super()
-        console.log('ChildComponent Constructor is called')
-    }
+    state = { count: 1 };
+
     render() {
-        console.log('ChildComponent render is called')
-
-        return <div>
-            <h1>Child Component</h1>
-            <SubChildComponent/>
-        </div>
+        console.log('render is called')
+        return (
+            <>
+                <button
+                    onClick={() => this.setState(state => ({ count: state.count + 1 }))}>
+                    Count: {this.state.count}
+                </button>
+                <button
+                    onClick={() => this.setState(state => ({ count: state.count }))}>
+                    Count: {this.state.count}
+                </button>
+            </>
+        );
     }
-    shouldComponentUpdate(nextProp, nextState) {
-        //biz logic : we compare props and state if it is same, return false else return true;
-        console.log('shouldComponentUpdate is called')
-        return true;
-    }
-
-    static getDerivedStateFromProps(props, state) {
-        console.log('ChildComponent getDerivedStateFromProps is called')
-        return null;
-    }
-    componentDidMount() {
-        console.log('ChildComponent ComponentDidMount is called')
-    }
-    componentDidUpdate(){
-        console.log('ChildComponent componentDidUpdate is called')
-
-    }
-
-
-
 }
-
-class SubChildComponent extends React.PureComponent {
-
-    state = {
-        j: 0
-    }
-    //mount phase methods
-    constructor() {
-        super()
-        console.log('SubChildComponent Constructor is called')
-    }
-    render() {
-        console.log('SubChildComponent render is called')
-
-        return <div>
-            <h1>SubChildComponent Component</h1>
-        </div>
-    }
-
-    static getDerivedStateFromProps(props, state) {
-        console.log('SubChildComponent getDerivedStateFromProps is called')
-        return null;
-    }
-    componentDidMount() {
-        console.log('SubChildComponent ComponentDidMount is called')
-    }
- 
-
-
-
-}
-ReactDOM.render(<ParentComponent />, document.getElementById('root'))
+ReactDOM.render(<>
+    <h3>Using ComponentShouldUpdate</h3>
+    <CounterButton />
+    <hr />
+    <h3>PureComponent</h3>
+    <CounterPureButton />
+</>
+    , document.getElementById('root'))
